33acd5609b237a03f812c1c9a781b852fbe26d68
Dasha <forenglishdash@list.ru>
2025-12-18 23:11:14 +0300

    create file and install l packages

---
+++ R/collect_data.R
@@ -0,0 +1,11 @@
+
+# МОДУЛЬ СБОРА ДАННЫХ ИЗ ARXIV ДЛЯ КИБЕРБЕЗОПАСНОСТИ
+
+library(httr)
+library(xml2)
+library(dplyr)
+library(stringr)
+library(tibble)
+library(lubridate)
+library(purrr)
+library(glue)
